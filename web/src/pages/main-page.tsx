895dd3a4d74d9520da9ec63de39f70efd2d09cd3
Ratthapon Wittayasuttikul <wittayasutt@gmail.com>
2025-09-15 21:36:04 +0700

    feat: add title "รวมลายแทง" to main page for improved user engagement

--- web/src/pages/main-page.tsx
+++ web/src/pages/main-page.tsx
@@ -4,6 +4,9 @@ function MainPage() {
 	return (
 		<div className="min-h-screen">
 			<div className="container">
+				<h1 className="mx-auto mb-16 mt-12 text-center text-4xl font-medium">
+					รวมลายแทง
+				</h1>
 				<GuideList />
 			</div>
 		</div>
